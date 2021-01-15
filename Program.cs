2c8ac8fa231d13f2c5b747e9b6b3a30e8cca83b2
kawan97 <77499820+kawan97@users.noreply.github.com>
2021-01-15 08:09:19 -0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Data_of_brth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string input = Console.ReadLine();
+            try { 
+                int age =int.Parse(input);
+                 if(age>0 && age <= DateTime.Now.Year)  { 
+                      age = DateTime.Now.Year - age;
+                         if(age !=0)
+                             Console.WriteLine("you are "+age + " years old");
+                           else
+                              Console.WriteLine("you are born in " + DateTime.Now.Year);
+                  }
+                      else{
+                           Console.WriteLine("Sorry you intered wrong number!");
+                       }
+             Console.ReadKey();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine("you inter a wrong charcarter");
+                Console.ReadKey();
+
+
+            }
+
+
+        }
+    }
+}
